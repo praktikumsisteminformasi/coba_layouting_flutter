e69d4b27f1369e7a934913ed570ac23c5407ce9b
novilfahlevy <novilfreon@gmail.com>
2024-03-18 22:18:55 +0800

    add info ride page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:coba_layouting/pages/home_page.dart';
+import 'package:coba_layouting/pages/info_ride_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,151 +19,10 @@ class AplikasiSaya extends StatelessWidget {
       theme: ThemeData(
         textTheme: GoogleFonts.poppinsTextTheme(), // Kita set font aplikasi kita pakai font 'Poppins' dari package GoogleFonts
       ),
-
-      // 'Scaffold' adalah Widget untuk membuat struktur aplikasi yang terdiri dari App Bar, Body, dan Bottom Navigation Bar
-      home: Scaffold(
-        // Ini App Bar, diisi menggunakan Widget 'AppBar'
-        appBar: AppBar(
-          title: const Text('INFORJEK', style: TextStyle(color: Colors.white),),
-          centerTitle: true,
-          backgroundColor: const Color(0xff4a8bc2),
-        ),
-
-        // Ini Body, isi utama dari aplikasi kita
-        // Kita pakai Widget 'ListView' agar halaman kita bisa di-scroll
-        body: ListView(
-          padding: const EdgeInsets.all(20),
-          children: [
-            // Bagian 'Informasi'
-            const Card(
-              color: Colors.blueAccent,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Informasi',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    SizedBox(height: 10,),
-                    Text(
-                      'Angkatan 2020 dapat diskon 20% untuk layanan Info Food.',
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-              ),
-            ),
-
-            // Kasih space kosong pakai Widget 'SizedBox'
-            const SizedBox(height: 20,),
-
-            // Tombol-tombol layanan
-            // karena tombol-tombol nya disusun secara horizontal, maka kita pakai Widget 'Row'
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Tombol layanan 'Info Ride'
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white
-                  ),
-                  child: const Text('Info Ride')
-                ),
-                // Tombol layanan 'Info Food'
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    foregroundColor: Colors.white
-                  ),
-                  child: const Text('Info Food')
-                ),
-                // Tombol layanan 'Info Send'
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange,
-                    foregroundColor: Colors.white
-                  ),
-                  child: const Text('Info Send')
-                ),
-              ],
-            ),
-
-            // Kasih space kosong pakai Widget 'SizedBox'
-            const SizedBox(height: 20,),
-
-            // Tempat, Makanan, dan Barang terbaik
-            // Karena card-card nya disusun secara vertikal, maka kita pakai Widget 'Column'
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // Tempat paling banyak dikunjungi
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Tempat paling banyak dikunjungi',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
-                        ),
-                        SizedBox(height: 10,),
-                        Text('Gedung Teknik Baru'),
-                      ],
-                    ),
-                  ),
-                ),
-
-                // Makanan paling banyak dipesan
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Makanan paling banyak dipesan',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
-                        ),
-                        SizedBox(height: 10,),
-                        Text('Ayam Ganja'),
-                      ],
-                    ),
-                  ),
-                ),
-
-                // Barang paling banyak dikirim
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Barang paling banyak dikirim',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)
-                        ),
-                        SizedBox(height: 10,),
-                        Text('Baju PDH Inforsa'),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        )
-      ),
+      routes: {
+        '/': (context) => const HomePage(),
+        '/info_ride': (context) => const InfoRidePage()
+      },
     );
   }
 }
---
+++ lib/pages/info_ride_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class InfoRidePage extends StatefulWidget {
+  const InfoRidePage({super.key});
+
+  @override
+  State<InfoRidePage> createState() => _InfoRidePageState();
+}
+
+class _InfoRidePageState extends State<InfoRidePage> {
+  bool isLoading = false;
+
+  void orderInfoRide() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Fetch HTTP disini
+    await Future.delayed(const Duration(seconds: 3));
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('INFORJEK', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: const Color(0xff4a8bc2),
+      ),
+      body: Form(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            // Titik jemput
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Titik jemput: Jl. Dari Sini No. 123'),
+                border: OutlineInputBorder()
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+
+            // Alamat tujuan
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Alamat tujuan: Jl. Kesini No. 321'),
+                border: OutlineInputBorder()
+              ),
+            ),
+
+            const SizedBox(height: 20,),
+
+            // Pesan
+            ElevatedButton(
+              onPressed: isLoading ? null : orderInfoRide,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+                padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30)
+              ),
+              child: Builder(
+                builder: (context) {
+                  if (isLoading) {
+                    return const Text('Memesan...', style: TextStyle(color: Colors.white),);
+                  }
+
+                  return const Row(
+                    children: [
+                      Icon(Icons.motorcycle, color: Colors.white, size: 28,), // Ini icon motor
+                      SizedBox(width: 20,),
+                      Text('Pesan sekarang', style: TextStyle(color: Colors.white),)
+                    ],
+                  );
+                }
+              )
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
